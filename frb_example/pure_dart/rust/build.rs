16a2293ee0eb4205e4d1071bcd0afdc08090411e
Roms1383 <romain.kelifa@gmail.com>
2022-09-28 16:04:33 +0700

    :ambulance: fix post-release

--- frb_example/pure_dart/rust/build.rs
+++ frb_example/pure_dart/rust/build.rs
@@ -10,6 +10,8 @@ const DART_OUTPUT: &str = "../dart/lib/bridge_generated.dart";
 fn main() {
     // Tell Cargo that if the input Rust code changes, to rerun this build script.
     println!("cargo:rerun-if-changed={}", RUST_INPUT);
+    println!("cargo:rustc-cfg=feature=\"chrono\"");
+    println!("cargo:rustc-cfg=feature=\"uuid\"");
     // Options for frb_codegen
     let raw_opts = RawOpts {
         // Path of input Rust code
